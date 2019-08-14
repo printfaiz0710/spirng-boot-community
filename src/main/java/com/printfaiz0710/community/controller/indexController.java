fb89ffdb62004198f6b9e927c3a9fe0f5e00fff4
printfaiz0710 <liuhaoxiang19980710@gmail.com>
2019-08-14 16:30:56 +0800

    add check cookies

--- src/main/java/com/printfaiz0710/community/controller/indexController.java
+++ src/main/java/com/printfaiz0710/community/controller/indexController.java
@@ -1,13 +1,33 @@
 package com.printfaiz0710.community.controller;
 
+import com.printfaiz0710.community.mapper.UserMapper;
+import com.printfaiz0710.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class indexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 
